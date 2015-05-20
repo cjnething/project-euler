0882670b1ec6d6afc4697c1ccea06dc8d9e98504
Julia Nething <cjnething@gmail.com>
2015-05-19 21:15:08 -0700

    (doc) Add completion status

--- Problem15.js
+++ Problem15.js
@@ -1,4 +1,5 @@
 // PROJECT EULER: PROBLEM 15 LATTICE PATHS
+//NOT COMPLETED
 
 // Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, 
 // there are exactly 6 routes to the bottom right corner.
