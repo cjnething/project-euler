130d2bc5932ba743d4bbb3d497181e59607e0701
Julia Nething <cjnething@gmail.com>
2015-05-26 22:10:13 -0700

    (feat) Worked on Problem 25, need to optimize so it doesn't crash because of the large number

--- Problem25.js
+++ Problem25.js
@@ -1,2 +1,33 @@
 //PROJECT EULER: PROBLEM 25 1000-DIGIT FIBONACCI NUMBER
 //NOT COMPLETED
+//DOES NOT WORK BECAUSE OF THE BIG INT
+
+var findIndex = function() {
+  var alreadyComputated = {};
+  var index = 1;
+  var found = false;
+
+  var fib = function(num) {
+    if(alreadyComputated[num-1] && alreadyComputated[num-2]) {
+      return alreadyComputated[num-1] + alreadyComputated[num-2];
+    } else {
+      if(num === 1 || num === 2) {
+        return 1;
+      } else {
+        return fib(num-1) + fib(num-2);
+      }
+    }
+  }
+
+  while (!found) {
+    alreadyComputated[index] = fib(index);
+    if(alreadyComputated[index].length === 1000) {
+      found = true;
+      return index;
+    } else {
+      index++;
+    }
+  }
+
+  return alreadyComputated;
+}
