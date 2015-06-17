7bb2367729e1ed54712d9edf74941989c57ca12d
Julia Nething <cjnething@gmail.com>
2015-06-17 10:54:17 -0700

    (feat) Completed Problem 15 Lattice Paths by redoing recursive function

--- 015-LatticePaths.js
+++ 015-LatticePaths.js
@@ -1,5 +1,7 @@
 //PROJECT EULER: PROBLEM 15 LATTICE PATHS
-//NOT COMPLETED
+//COMPLETED 6/17/15
+//ANSWER: 137846528820
+
 
 // Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, 
 // there are exactly 6 routes to the bottom right corner.
@@ -8,90 +10,43 @@
 
 
 
-var findPaths = function(nRow, nCol) {
-  var alreadyFound = {};
-  var routes = 1;
+var findPaths = function(n) {
+  var alreadyFound = [];
+  var routes = 0;
 
-  for (var index = 0; index < nRow; index++) {
-    alreadyFound[index] = {};
+  for (var i = 0; i<=n; i++) {
+    alreadyFound.push([]);
+    for (var j = 0; j<=n; j++) {
+      alreadyFound[i].push(false);
+    }
   }
 
-  var recurse = function(i, j) {
-    if (i === nRow || j === nCol) {
-      return routes;
+  alreadyFound[0][0] = 0;
+
+  var recurse = function(row, col) {
+    if (row === 0 || col === 0) {
+      alreadyFound[row][col] = 1;
+      routes++;
+      return alreadyFound[row][col];
     }
-    else if (alreadyFound[i][j]) {
-      //return routes + alreadyFound[i][j];
-      return alreadyFound[i][j];
+    else if (alreadyFound[row][col]) {
+      routes += alreadyFound[row][col]
+      return alreadyFound[row][col];
     }
     else {
-      routes++;
-      if (i+1 < nRow) {
-         var temp1 = findPaths(nRow - (i+1), nCol - j);
-         alreadyFound[i+1][j] = temp1;
-         routes += temp1;
-      }
-      if (j+1 < nCol) {
-         var temp2 = findPaths(nRow - i, nCol - (j+1));
-         alreadyFound[i][j+1] = temp2;
-         routes += temp2;
-      }
-      if (i+1 === nRow || j+1 === nCol) {
-        return routes;
-      }
-      
+      alreadyFound[row][col] = recurse(row-1, col) + recurse(row, col-1);
+      return alreadyFound[row][col];
     }
   }
 
-  recurse(0, 0);
-
-  console.log(alreadyFound);
-  return routes;
+  return recurse(n, n);
 }
 
-
-findPaths(2);
-
+console.log(findPaths(20))
 
 
+// Congratulations, the answer you gave to problem 15 is correct.
+
+// You are the 107115th person to have solved this problem.
 
 
-
-
-// var findPaths = function(n) {
-//   var alreadyFound = {};
-//   var alreadyChecked = {};
-//   var routes = 1;
-  
-//    var recurse = function(i, j) {
-//      if(i === n || j === n) {
-//          return routes;
-//      } else if (i > 0 && i === j) {
-//         if(alreadyChecked[i]) {
-//             return routes;
-//         }
-//         else if(alreadyFound[n-i]) {
-//             routes += alreadyFound[n-i];
-//         } else {
-//             var temp = findPaths(n-i);
-//             alreadyFound[n-i] = temp;
-//             routes += temp;
-//         }
-//         alreadyChecked[i] = true;
-//         return routes;
-//      } else {
-//          routes++;
-//          recurse(i+1, j);
-//          recurse(j, i+1);
-//      }
-     
-//    }
-   
-//    recurse(0,0);
-//   return routes;
-// }
-
-
-for (var i = 1; i<15; i++) {
-  console.log('i', i, 'paths', findPaths(i));
-}
