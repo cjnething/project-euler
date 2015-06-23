cf5f5c4725ca2e5cf28e063af07ca6adfb5e7511
Julia Nething <cjnething@gmail.com>
2015-06-23 18:00:08 -0400

    (feat) Make a new tactic for Concealed Squares, still doesn't work

--- 164-NumbersNoThreeConsecDigits.js
+++ 164-NumbersNoThreeConsecDigits.js
@@ -5,26 +5,67 @@
 //How many 20 digit numbers n (without any leading zero) exist such that no three consecutive digits of n have a sum greater than 9?
 
 
-function findCount() {
-  var count = 0;
-  var digits = [];
-  var applicable = true;
+// function findCount() {
+//   var count = 0;
+//   var digits = [];
+//   var applicable = true;
 
-  for (var i = 10000000000000000000; i<=99999999999999999999; i++) {
-    digits = i.toString().split("");
-    for (var start = 0; start<18; start++) {
-      if (Number(digits[start]) + Number(digits[start+1]) + Number(digits[start+2]) > 9) {
-        applicable = false;
-        break;
+//   for (var i = 100; i<910; i++) {
+//     digits = i.toString().split("");
+//     for (var start = 0; start<18; start++) {
+//       if (Number(digits[start]) + Number(digits[start+1]) + Number(digits[start+2]) > 9) {
+//         applicable = false;
+//         break;
+//       }
+//     }
+//     if (applicable) {
+//       console.log(i);
+//       count++;
+//     }
+//     applicable = true;
+//   }
+//   return count;
+// }
+
+
+// console.log(findCount());
+
+
+function findResult() {
+  var currCount = 0;
+  var countResult = 0;
+  var current = "";
+
+  function recurse(curr) {
+    console.log(curr);
+    if (curr.length === 5) {
+      countResult++;
+      return;
+    }
+    else if (curr.length === 0) {
+      currCount = 0;
+    } else if (curr.length === 1) {
+      currCount = Number(curr);
+    } else {
+      currCount = Number(curr.charAt(curr.length-2)) + Number(curr.charAt(curr.length-1));
+    }
+
+
+    for (var i = 0; i<=9; i++) {
+      if (curr.length !== 0 || i !== 0) {
+        if (currCount + i <= 9) {
+          curr = curr.concat(i);
+          console.log(curr)
+          recurse(curr);
+        }
       }
+
     }
-    if (applicable) {
-      count++;
-    }
-    applicable = true;
   }
-  return count;
+
+  recurse(current);
+  
+  return countResult;
 }
 
-
-console.log(findCount());
+console.log(findResult());
--- 206-ConcealedSquare.js
+++ 206-ConcealedSquare.js
@@ -4,22 +4,44 @@
 //Find the unique positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0,
 //where each “_” is a single digit.
 
-function findInt() {
-  var found = false;
-  var i = 1010101030;
-  var target = 0;
+// function findInt() {
+//   var found = false;
+//   var i = 1010101030;
+//   var target = 0;
 
-  while(!found || (i*i) < 1999999999999999999) {
-    var squared = (i*i).toString();
-    console.log('i', i, 'squared', squared);
-    if(squared[0] === "1" || squared[2] === "2" || squared[4] === "3" || 
-      squared[6] === "4" || squared[8] === "5" || squared[10] === "6" || 
-      squared[12] === "7" || squared[14] === "8" || squared[16] === "9" ||
-      squared[18] === "0") {
-      found = true;
-      target = i;
-      return target;
-    }
-    i += 30;
+//   while(!found || (i*i) < 1999999999999999999) {
+//     var squared = (i*i).toString();
+//     console.log('i', i, 'squared', squared);
+//     if(squared[0] === "1" || squared[2] === "2" || squared[4] === "3" || 
+//       squared[6] === "4" || squared[8] === "5" || squared[10] === "6" || 
+//       squared[12] === "7" || squared[14] === "8" || squared[16] === "9" ||
+//       squared[18] === "0") {
+//       found = true;
+//       target = i;
+//       return target;
+//     }
+//     i += 30;
+//   }
+// }
+
+
+function findInt() {
+  var squared;
+
+  //Math.ceil(Math.sqrt(1020304050607080900)) = 1010101011
+  //Math.floor(Math.sqrt(1929394959697989990)) = 1389026623
+
+  for (var i = 1010101011; i<1110101011; i++) {
+    squared = Math.pow(i, 2).toString();
+    if(squared[18] === "0" && squared[16] === "9" && squared[14] === "8" && 
+      squared[12] === "7" && squared[10] === "6" && squared[8] === "5" && 
+      squared[6] === "4" && squared[2] === "2" && squared[4] === "3" && squared[0] === "1") {
+      return i;
   }
 }
+}
+
+
+console.log(findInt());
+//console.log(Math.floor(Math.sqrt(1929394959697989990)));
+//45.9 seconds, 41.1
