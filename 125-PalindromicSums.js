39bdc8ff2e887671b6c122a9e28a65129ffb93d4
Julia Nething <cjnething@gmail.com>
2015-06-23 15:51:21 -0400

    (feat) Complete Problem 125 Palindromic Sums

--- 125-PalindromicSums.js
+++ 125-PalindromicSums.js
@@ -1,5 +1,6 @@
 //PROJECT EULER: PROBLEM 125 PALINDROMIC SUMS
-//NOT COMPLETED
+//COMPLETED 6/23/15
+//ANSWER: 2906969179
 
 
 // The palindromic number 595 is interesting because it can be written as the sum of consecutive squares: 6^2 + 7^2 + 8^2 + 9^2 + 10^2 + 11^2 + 12^2.
@@ -7,7 +8,6 @@
 // There are exactly eleven palindromes below one-thousand that can be written as consecutive square sums, and the sum of these palindromes is 4164. Note that 1 = 0^2 + 1^2 has not been included as this problem is concerned with the squares of positive integers.
 
 // Find the sum of all the numbers less than 10^8 that are both palindromic and can be written as the sum of consecutive squares.
-
 
 
 
@@ -29,42 +29,31 @@ function palindromicSums(limit) {
   var countSum = 0;
   var first = 1;
   var second = 2;
+  var checked = {};
 
-  while (first < Math.sqrt(limit)) {
+  while (first <= Math.sqrt(limit)/2) {
     if (sum > limit) {
       first++;
       second = first + 1;
       sum = Math.pow(first, 2);
     }
     sum += Math.pow(second, 2);
-    if (sum < limit) {
-      if (isPalindromic(sum)) {
-        count++;
-        countSum += sum;
-        console.log(first, second, sum);
-      }
+    if (sum < limit && isPalindromic(sum) && !checked[sum]) {
+      count++;
+      countSum += sum;
+      checked[sum] = true;
     }
    
     second++;
   }
   
-  // for (var i = 1; i<limit; i++) {
-  //     sum += Math.pow(i, 2)
-  //   for (var j = i+1; j<limit; j++) {
-  //     sum += Math.pow(j, 2);
-  //     if (isPalindromic(sum)) {
-  //       count++;
-  //       countSum += sum;
-  //       console.log(i, j, sum);
-  //     }
-  //   }
-
-  //   sum = 0;
-  // }
-
-  console.log(count);
   return countSum;
 }
 
+console.log(palindromicSums(100000000));
 
-console.log(palindromicSums(Math.pow(10, 8)))
+
+// Congratulations, the answer you gave to problem 125 is correct.
+
+// You are the 8544th person to have solved this problem.
+
