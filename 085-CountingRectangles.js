2d75f5d9eeb60735881d99405781390fbe1ab776
Julia Nething <cjnething@gmail.com>
2015-07-11 22:16:42 -0700

    (feat) Begin Counting Rectangles problem

---
+++ 085-CountingRectangles.js
@@ -0,0 +1,20 @@
+//PROJECT EULER: PROBLEM 85 COUNTING RECTANGLES
+//NOT COMPLETED
+
+
+// By counting carefully it can be seen that a rectangular grid measuring 3 by 2 contains eighteen rectangles:
+
+// Although there exists no rectangular grid that contains exactly two million rectangles, find the area of the grid with the nearest solution.
+
+
+var countRectangles = function(height, width) {
+   var count = 0;
+   var area = height * width;
+
+   for (var i = 1; i<=height; i++) {
+     for (var j = 1; j<=width; j++) {
+       count += 
+     }
+   }
+
+}
