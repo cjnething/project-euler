d3793944d1b51078bf864372b988a357e0a641ca
Julia Nething <cjnething@gmail.com>
2017-05-18 19:47:09 -0700

    (feat) Write problem description for Problem 135

---
+++ 135-SameDifferences.js
@@ -0,0 +1,13 @@
+//PROJECT EULER: PROBLEM 135 SAME DIFFERENCES
+//NOT COMPLETED
+
+/*
+Given the positive integers, x, y, and z, are consecutive terms of an arithmetic progression, the least value of the positive integer, n, for which the equation, x2 − y2 − z2 = n, has exactly two solutions is n = 27:
+
+34^2 − 27^2 − 20^2 = 12^2 − 9^2 − 6^2 = 27
+
+It turns out that n = 1155 is the least value which has exactly ten solutions.
+
+How many values of n less than one million have exactly ten distinct solutions?
+*/
+
